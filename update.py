c913f528c4425446b7a446e10e1155914a1f790b
Tobias Nauen <tobias.nauen@gmail.com>
2025-05-30 12:26:04 +0200

    Update conference data at 2025-05-30T12:26:04.709414

---
+++ update.py
@@ -0,0 +1,19 @@
+import os
+from datetime import datetime
+
+this_dir = os.path.dirname(__file__)
+
+update_script = os.path.join(this_dir, "src", "update_data.py")
+os.system(f"python3 {update_script} --online")
+
+to_json_script = os.path.join(this_dir, "src", "data_to_json.py")
+
+git_tkn_file = os.path.join(this_dir, ".gittkn")
+if os.path.isfile(git_tkn_file):
+    with open(git_tkn_file, "r") as f:
+        git_tkn = f.read().strip()
+    os.system(f"git pull https://tobna:{git_tkn}@github.com/tobna/ai-deadlines")
+
+    os.system("git add .")
+    os.system(f"git commit -m 'Update conference data at {datetime.now().isoformat()}'")
+    os.system(f"git push https://tobna:{git_tkn}@github.com/tobna/ai-deadlines")
