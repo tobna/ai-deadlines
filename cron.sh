176651e66912d07011dec15a2ff9cc86f31fbe6d
Tobias Nauen <tobias.nauen@gmail.com>
2025-05-30 13:05:18 +0200

    Update conference data at 2025-05-30T13:05:18.379095

--- cron.sh
+++ cron.sh
@@ -3,6 +3,6 @@
 BASEDIR=$(dirname $0)
 echo "starting cron job for script ${BASEDIR}/update.py"
 while true; do
-  python3 ${BASEDIR}/update.py
+  /bin/bash ${BASEDIR}/make_website.sh
   sleep 60 * 60 * 10
 done
---
+++ make_website.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+OLD_BASEDIR=$(PWD)
+BASEDIR=$(dirname $0)
+echo "updating and creating website"
+
+echo "goto ${BASEDIR}"
+cd BASEDIR
+
+echo "1. update conference data"
+python3 ${BASEDIR}/update.py
+
+echo "2. compress files: gzip"
+
+find src -type f \( -name '*.html' -o -name '*.js' -o -name '*.css' -o -name '*.ttf' -o -name '*.woff2' -o -name '*.xml' -o -name '*.svg' -o -name '*.jpg' -o -name '*.webp' \) -exec gzip -v -k -f --best {} \;
+
+echo "3. copy files over"
+cp -r src /usr/share/nginx/html/
+
+echo "4. remove changes to git repo"
+git stash
+
+echo "goto ${OLD_BASEDIR}"
+cd $(OLD_BASEDIR)
