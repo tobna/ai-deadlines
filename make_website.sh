ba7ed6721b4eab69d639ec38df9945df9593c368
Tobias Nauen <tobias.nauen@gmail.com>
2025-05-30 14:46:49 +0200

    also compress jsons

--- make_website.sh
+++ make_website.sh
@@ -14,7 +14,7 @@ python3 data_to_json.py
 
 echo "3. compress files: gzip"
 
-find src -type f \( -name '*.html' -o -name '*.js' -o -name '*.css' -o -name '*.ttf' -o -name '*.woff2' -o -name '*.xml' -o -name '*.svg' -o -name '*.jpg' -o -name '*.webp' \) -exec gzip -v -k -f --best {} \;
+find src -type f \( -name '*.json' -name '*.html' -o -name '*.js' -o -name '*.css' -o -name '*.ttf' -o -name '*.woff2' -o -name '*.xml' -o -name '*.svg' -o -name '*.jpg' -o -name '*.webp' \) -exec gzip -v -k -f --best {} \;
 
 echo "4. copy files over"
 cp -r src/* /usr/share/nginx/html/
