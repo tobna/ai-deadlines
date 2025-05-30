389f4097f06f1c29b6c6e10862d5bc258b662d6c
Tobias Nauen <tobias.nauen@gmail.com>
2025-05-30 13:26:35 +0200

    wait for initial update

--- cron.sh
+++ cron.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "starting cron job for script ./website/update.py"
+echo "starting cron job for script ./website/update.py in 60s"
+sleep 60
 while true; do
   ./website/make_website.sh
   wait
