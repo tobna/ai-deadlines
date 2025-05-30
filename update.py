df53c049a76af094b9d94d69f8163a306d1a0116
Tobias Nauen <tobias.nauen@gmail.com>
2025-05-30 12:28:02 +0200

    pull before auto update

--- update.py
+++ update.py
@@ -3,17 +3,20 @@ from datetime import datetime
 
 this_dir = os.path.dirname(__file__)
 
-update_script = os.path.join(this_dir, "src", "update_data.py")
-os.system(f"python3 {update_script} --online")
-
-to_json_script = os.path.join(this_dir, "src", "data_to_json.py")
-
 git_tkn_file = os.path.join(this_dir, ".gittkn")
 if os.path.isfile(git_tkn_file):
     with open(git_tkn_file, "r") as f:
         git_tkn = f.read().strip()
     os.system(f"git pull https://tobna:{git_tkn}@github.com/tobna/ai-deadlines")
+else:
+    git_tkn = None
 
+update_script = os.path.join(this_dir, "src", "update_data.py")
+os.system(f"python3 {update_script} --online")
+
+to_json_script = os.path.join(this_dir, "src", "data_to_json.py")
+
+if git_tkn is not None:
     os.system("git add .")
     os.system(f"git commit -m 'Update conference data at {datetime.now().isoformat()}'")
     os.system(f"git push https://tobna:{git_tkn}@github.com/tobna/ai-deadlines")
