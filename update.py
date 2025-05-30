82b92c156e6c06045537821a4adb20edf83f5d18
Tobias Nauen <tobias.nauen@gmail.com>
2025-05-30 13:15:25 +0200

    print normally

--- update.py
+++ update.py
@@ -12,8 +12,8 @@ else:
     git_tkn = None
 
 update_script = os.path.join(this_dir, "src", "update_data.py")
-autoupdate_log = os.path.join(this_dir, "autoupdate.log")
-os.system(f"python3 {update_script} --online > {autoupdate_log} 2>&1")
+# autoupdate_log = os.path.join(this_dir, "autoupdate.log")
+os.system(f"python3 {update_script} --online")
 
 to_json_script = os.path.join(this_dir, "src", "data_to_json.py")
 
