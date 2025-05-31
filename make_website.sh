316716308955ef8a9b0fc02c608c3ca2210726cb
Tobias Nauen <tobias.nauen@gmail.com>
2025-05-31 11:27:30 +0200

    Update conference data at 2025-05-31T11:27:30.191153

--- make_website.sh
+++ make_website.sh
@@ -9,12 +9,12 @@ ls
 echo "1. update conference data"
 python3 update.py
 
-echo "2. compress files: gzip"
+echo "2. copy files over"
+cp -r src/* html/
 
-find src -type f \( -name '*.json' -name '*.html' -o -name '*.js' -o -name '*.css' -o -name '*.ttf' -o -name '*.woff2' -o -name '*.xml' -o -name '*.svg' -o -name '*.jpg' -o -name '*.webp' \) -exec gzip -v -k -f --best {} \;
+echo "3. compress files: gzip"
 
-echo "3. copy files over"
-cp -r src/* /usr/share/nginx/html/
+find html -type f \( -name '*.json' -name '*.html' -o -name '*.js' -o -name '*.css' -o -name '*.ttf' -o -name '*.woff2' -o -name '*.xml' -o -name '*.svg' -o -name '*.jpg' -o -name '*.webp' \) -exec gzip -v -k -f --best {} \;
 
 echo "4. remove changes to git repo"
 git stash
