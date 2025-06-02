4092e0d3f928bfb8e88d53dbe20307d3f4150073
Tobias Nauen <53979841+tobna@users.noreply.github.com>
2025-06-02 18:05:39 +0200

    make tailwind executable

--- update_tailwind.sh
+++ update_tailwind.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 wget -nc https://github.com/tailwindlabs/tailwindcss/releases/download/v4.1.8/tailwindcss-linux-x64
+chmod +x tailwindcss-linux-x64
 
 ./tailwindcss-linux-x64 -i src/styles.css -o src/styles.tailwind.css --content "./*.{html,js}" -m
