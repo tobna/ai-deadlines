5d56928f267b88bd68a32b4b1eb5980e3a0ccdc2
Tobias Christian Nauen <tobias_christian.nauen@dfki.de>
2026-02-23 14:41:14 +0100

    correct folder name

--- make_website.sh
+++ make_website.sh
@@ -11,7 +11,10 @@ echo "2. update tailwindcss"
 ./update_tailwind.sh
 
 echo "3. copy files over"
-cp -r src/* html/
+mkdir -p html/data
+cp aideadlines/*.html aideadlines/*.js aideadlines/*.css html/
+cp aideadlines/data/*.json html/data/
+cp aideadlines/*.ico aideadlines/*.svg aideadlines/*.png aideadlines/site.webmanifest aideadlines/robots.txt aideadlines/sitemap.xml html/
 
 echo "4. compress files: gzip"
 find html -type f \( -name '*.json' -o -name '*.html' -o -name '*.js' -o -name '*.css' -o -name '*.ttf' -o -name '*.woff2' -o -name '*.xml' -o -name '*.svg' -o -name '*.jpg' -o -name '*.webp' \) -exec gzip -v -k -f --best {} \;
--- update_tailwind.sh
+++ update_tailwind.sh
@@ -3,4 +3,4 @@
 wget -nc https://github.com/tailwindlabs/tailwindcss/releases/download/v4.1.8/tailwindcss-linux-x64
 chmod +x tailwindcss-linux-x64
 
-./tailwindcss-linux-x64 -i src/styles.css -o src/styles.tailwind.css --content "./*.{html,js}" -m
+./tailwindcss-linux-x64 -i aideadlines/styles.css -o aideadlines/styles.tailwind.css --content "./*.{html,js}" -m
