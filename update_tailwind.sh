25575c2caa4fee242125ad742490e48b8b589ee4
Tobias Christian Nauen <tobias_christian.nauen@dfki.de>
2026-02-26 12:19:47 +0100

    force rewrite of tailwind css

--- update_tailwind.sh
+++ update_tailwind.sh
@@ -3,4 +3,5 @@
 wget -nc https://github.com/tailwindlabs/tailwindcss/releases/download/v4.1.8/tailwindcss-linux-x64
 chmod +x tailwindcss-linux-x64
 
+rm -f aideadlines/styles.tailwind.css
 ./tailwindcss-linux-x64 -i aideadlines/styles.css -o aideadlines/styles.tailwind.css --content "./*.{html,js}" -m
