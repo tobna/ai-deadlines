5e368a9abdd5a5c286ad8d9308a49b9823bd09a3
Tobias Nauen <tobias.nauen@gmail.com>
2026-03-30 10:15:45 +0200

    website fallback to GMT-12  (= Etc/GMT+12)

--- aideadlines/data_to_json.py
+++ aideadlines/data_to_json.py
@@ -27,26 +27,24 @@ future_conf = {}
 past_conf = {}
 
 for id, conf in conferences.items():
-    if "timezone" in conf:
-        js_tz = conf["timezone"]
-        old_tz = js_tz
-        if "UTC" in js_tz:
-            if "+" in js_tz:
-                char = "+"
-            elif "-" in js_tz:
-                char = "-"
-            else:
-                char = None
+    js_tz = conf.get("timezone", "AoE")
+    old_tz = js_tz
+    if "UTC" in js_tz:
+        if "+" in js_tz:
+            char = "+"
+        elif "-" in js_tz:
+            char = "-"
+        else:
+            char = None
 
-            if char is not None:
-                old_tz = js_tz
-                offset = int(js_tz.split(char)[-1])
-                js_tz = f"Etc/GMT{'+' if char == '-' else '-'}{offset}"
-        js_tz = js_tz.replace("AoE", "Etc/GMT+12").replace("Russia/Moscow", "Etc/GMT+3")
-        if old_tz != js_tz:
-            logger.debug(f"Parsing timezone for json: {old_tz} -> {js_tz}")
-
-        conf["timezone"] = js_tz
+        if char is not None:
+            old_tz = js_tz
+            offset = int(js_tz.split(char)[-1])
+            js_tz = f"Etc/GMT{'+' if char == '-' else '-'}{offset}"
+    js_tz = js_tz.replace("AoE", "Etc/GMT+12").replace("Russia/Moscow", "Etc/GMT+3")
+    if old_tz != js_tz:
+        logger.debug(f"Parsing timezone for json: {old_tz} -> {js_tz}")
+    conf["timezone"] = js_tz
     for i, dates in enumerate(conf["timeline"]):
         conf_cpy = deepcopy(conf)
         conf_cpy.pop("timeline")
